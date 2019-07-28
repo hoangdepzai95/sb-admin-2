367509c0af898effcb458749ae2ff61e97fb6d7f
root <root@mensr.store.co>
2019-07-28 18:26:14 +0700

    d

--- server/server/db.js
+++ server/server/db.js
@@ -3,10 +3,10 @@ const mysql = require('mysql');
 
 function createPool() {
   const pool = mysql.createPool({
-    host     : '103.63.109.151',
+    host     : '103.63.109.219',
     user     : 'monty',
     password : 'Kidvn95123@',
-    database : 'hoang-app',
+    database : 'men',
     connectionLimit: 1000,
   });
   return pool;
